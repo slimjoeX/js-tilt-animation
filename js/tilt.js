5d914d405e703e4a7cc9177bb47f42423c8779b9
Steve R <retroslimjoe@hotmail.com>
2019-10-08 17:46:40 -0400

    Script initiated

---
+++ js/tilt.js
@@ -0,0 +1,10 @@
+function tilt(element) {
+    element.on("mousemove", function(e) {
+        var x = e.offsetX;
+        var y = e.offsetY;
+        // var coor = "Coordinates: (" + x + "," + y + ")";
+        // console.log(coor);
+
+
+    });
+}
